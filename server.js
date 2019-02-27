84082f9e27574d45152a3ced7c1e3bf7f2cdfb29
Claudio Mamani <claudio.mamani@globant.com>
2019-02-27 11:25:44 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req,res){
+    return res.send('test route');
+})
+
+app.get('/test', function(req,res){
+    return res.send('another route');
+})
+
+app.get('/test1', function(req,res){
+    return res.send('another route1');
+})
+
+app.listen(4000, function(){
+    console.log('server running on port 4000');
+})
